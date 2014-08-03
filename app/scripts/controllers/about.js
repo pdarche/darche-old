0b852eb7e5e1a3a293af642b851caf3c52ea91aa
Peter Darche <pdarche@gmail.com>
2014-08-02 20:19:16 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name darcheApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the darcheApp
+ */
+angular.module('darcheApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name darcheApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the darcheApp
+ */
+angular.module('darcheApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS'
+    ];
+  });
