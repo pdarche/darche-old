a771695582e1fb15e84287f610ac16d75c6bfaf4
Peter Darche <pdarche@gmail.com>
2014-09-07 12:00:09 -0400

    Minor blog changes

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -10,9 +10,9 @@
 angular.module('darcheApp')
   .controller('BlogCtrl', function ($scope) {
     $scope.posts = [
-      {title: 'Test', subTitle: 'Test Subtitle', postTime: '2014-09-06', body:'Some really cool stuff', tags:['one', 'two', 'three']},
-      {title: 'Test', subTitle: 'Test Subtitle', postTime: '2014-09-06', body:'Some really cool stuff', tags:['one', 'two', 'three']},
-      {title: 'Test', subTitle: 'Test Subtitle', postTime: '2014-09-06', body:'Some really cool stuff', tags:['one', 'two', 'three']}
+      {title: 'Test', subTitle: 'Test Subtitle', postTime: '2014-09-06', body:'Some really cool stuff', tags: ['one', 'two', 'three']},
+      {title: 'Test', subTitle: 'Test Subtitle', postTime: '2014-09-06', body:'Some really cool stuff', tags: ['one', 'two', 'three']},
+      {title: 'Test', subTitle: 'Test Subtitle', postTime: '2014-09-06', body:'Some really cool stuff', tags: ['one', 'two', 'three']}
     ];
 
     $scope.stuffs = [
