37c29b7d6af8a7c3bc692643c1c13e61a6291a03
Peter Darche <pdarche@gmail.com>
2014-09-20 17:01:39 -0400

    Create nav bar directive

---
+++ app/scripts/directives/left-nav.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc left-nav
+ * @name 
+ * @description
+ * # darcheApp
+ *
+ * 
+ */
+
+angular
+  .module('pdDirectives', [])
+  .directive('pdLeftNav', function(){
+    return {  
+      templateUrl: 'views/nav.html',
+      link: function(scope, el, attrs){
+        el.on('click', 'img', function(ev){
+          var menu = $('#nav_menu');
+          
+          menu.hasClass('hidden') ? 
+            menu.removeClass('hidden') :
+            menu.addClass('hidden');
+        });
+
+        el.on('mouseenter', 'img', function(ev){
+          $(this).addClass('hovered')
+        }).on('mouseout', 'img', function(){
+          $(this).removeClass('hovered')
+        })
+      }
+    }
+  });
