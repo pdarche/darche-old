d9076d62b8ac622a483103a0f565e10bc8f77788
Peter Darche <pdarche@gmail.com>
2014-11-07 21:24:03 -0500

    Add draft controller

---
+++ app/scripts/controllers/draft.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name darcheApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the darcheApp
+ */
+angular.module('darcheApp')
+  .controller('DraftCtrl', function ($scope, $http, Post) {
+    var posts = Post.query({publish:true}, function(){
+      // I want this to be intercepted!
+      $scope.posts = posts.rows.map(function(row){ return row.doc })
+                        .filter(function(doc){ return doc.publish !== true});
+    })
+  });
