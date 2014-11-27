2b845ad8929589fd6f134446f2e58eac31c96c6a
Peter Darche <pdarche@gmail.com>
2014-11-27 12:01:33 -0500

    Remove ugilification to address deployment issue.  For more permanent fix, see: https://github.com/gruntjs/grunt-contrib-uglify#no-mangling

--- Gruntfile.js
+++ Gruntfile.js
@@ -226,7 +226,7 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat', 'uglifyjs'],
+              js: ['concat'], //'uglifyjs'],
               css: ['cssmin']
             },
             post: {}
@@ -431,7 +431,7 @@ module.exports = function (grunt) {
     'copy:dist',
     'cdnify',
     'cssmin',
-    'uglify',
+    // 'uglify',
     'filerev',
     'usemin',
     'htmlmin'
