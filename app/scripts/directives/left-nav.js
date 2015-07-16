0385f1187918d43961d76ad3ab6d0f05dfe90ab8
Peter Darche <pdarche@gmail.com>
2015-07-16 00:13:38 -0400

    Update nav markup and styling.

--- app/scripts/directives/left-nav.js
+++ app/scripts/directives/left-nav.js
@@ -2,25 +2,25 @@
 
 /**
  * @ngdoc left-nav
- * @name 
+ * @name
  * @description
  * # darcheApp
  *
- * 
+ *
  */
 
 angular
   .module('pdDirectives', [])
   .directive('pdLeftNav', function(){
-    return {  
+    return {
       templateUrl: 'views/nav.html',
       link: function(scope, el, attrs){
-        el.on('click', 'img', function(){
-          var menu = $('#nav_menu');
-          
-          menu.hasClass('hidden') ? 
-            menu.removeClass('hidden') : 
-            menu.addClass('hidden');
+        el.on('click', '#nav_icon', function(ev){
+          var menu = $('#nav_bar');
+
+          menu.hasClass('not-shown') ?
+            menu.removeClass('not-shown') :
+            menu.addClass('not-shown');
         });
       }
     }
