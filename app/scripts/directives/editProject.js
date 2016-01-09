151535315be64e900d26af432cf25ddc1557edd9
Peter Darche <pdarche@gmail.com>
2016-01-08 21:26:28 -0500

    Update save button for posts and projects

--- app/scripts/directives/editProject.js
+++ app/scripts/directives/editProject.js
@@ -15,6 +15,24 @@ angular
       restrict: 'AEC',
       templateUrl: 'views/partials/editProject.html',
       link: function(scope, el, attrs){
+        el.on('click', '.editor-save', function(ev){
+          ev.preventDefault();
+
+          el.find('.editor-save__icon').css({'background-image':'url("/images/ripple.gif")'});
+
+          var updateTime = new Date().getTime();
+          scope.project.updates.push(updateTime);
+          scope.project.lastUpdated = updateTime;
+
+          Project.update({id: scope.project._id}, scope.project, function(data){
+              scope.project._rev = data.rev;
+              el.find('.editor-save__icon').css({'background-image':''});
+            }, function(err){
+              el.find('.editor-save__icon').css({'background-image':''});
+              alert("Error!");
+            });
+        });
+
         el.on('click', '#preview', function(ev){
           ev.preventDefault();
 
